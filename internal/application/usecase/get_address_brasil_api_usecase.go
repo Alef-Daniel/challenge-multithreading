0b519a74afe87fc65645bf121ff6d8b09bb99e87
Alef Sousa Aguiar Daniel <83924026+Alef-Daniel@users.noreply.github.com>
2026-04-13 23:42:49 -0300

    Feature/implementation apis external (#2)
    * feat: create domain struct
    
    * feat: create usecase implemntation make request viacep API
    
    * feat: create usecase implemntation make request Brasil API
    
    * feat: create httpClient constructor

---
+++ internal/application/usecase/get_address_brasil_api_usecase.go
@@ -0,0 +1,119 @@
+package usecase
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/alef-daniel/challenge-multithreading/internal/application/domain"
+	"github.com/alef-daniel/challenge-multithreading/pkg"
+)
+
+var (
+	ErrBrasilAPI = errors.New("Brasil api error")
+)
+
+type GetAddressBrasilAPIUseCase struct {
+	client pkg.Client
+}
+
+func (g *GetAddressBrasilAPIUseCase) GetAddress(ctx context.Context, cep string) (*domain.Address, error) {
+	if cep == "" {
+		return nil, ErrCepIsEmpty
+	}
+
+	url := g.BuildURL(ctx, cep)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := g.client.Http.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Error make request: %w", err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 0 && resp.StatusCode != http.StatusOK {
+		switch resp.StatusCode {
+		case http.StatusNotFound:
+			return nil, ErrCepNotFound
+		default:
+			return nil, ErrBrasilAPI
+		}
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error read response body: %w", err)
+	}
+
+	address, err := g.BuildResponse(ctx, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return address, nil
+
+}
+
+func (g *GetAddressBrasilAPIUseCase) BuildURL(ctx context.Context, cep string) string {
+	return fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", cep)
+}
+
+func (g *GetAddressBrasilAPIUseCase) BuildResponse(ctx context.Context, response []byte) (*domain.Address, error) {
+	responseMap := make(map[string]interface{})
+	if len(response) == 0 {
+		return nil, ErrBrasilAPI
+	}
+
+	address := &domain.Address{}
+	err := json.Unmarshal(response, &responseMap)
+	if err != nil {
+		return nil, err
+	}
+
+	if responseMap["cep"] != nil {
+		cep, ok := responseMap["cep"].(string)
+		if !ok {
+			return nil, ErrInvalidTypeData
+		}
+		address.Cep = cep
+	}
+
+	if responseMap["logradouro"] != nil {
+		logradouro, ok := responseMap["logradouro"].(string)
+		if !ok {
+			return nil, ErrInvalidTypeData
+		}
+		address.Logradouro = logradouro
+	}
+
+	if responseMap["uf"] != nil {
+		uf, ok := responseMap["uf"].(string)
+		if !ok {
+			return nil, ErrInvalidTypeData
+		}
+
+		address.UF = uf
+	}
+
+	if responseMap["Bairro"] != nil {
+		bairro, ok := responseMap["Bairro"].(string)
+		if !ok {
+			return nil, ErrInvalidTypeData
+		}
+		address.Bairro = bairro
+	}
+
+	return address, nil
+
+}
+
+func NewGetAddressBrasilAPIUseCase(client pkg.Client) *GetAddressBrasilAPIUseCase {
+	return &GetAddressBrasilAPIUseCase{client: client}
+}
